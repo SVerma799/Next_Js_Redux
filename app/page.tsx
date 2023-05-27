88c6e64c3d005c303e4474c3d3ca41263905945a
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-05-27 01:03:06 -0700

    Changes for adding button to the component

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import { increament, decrement } from "@/slice/counterSlice";
 import type { RootState } from "./store";
 export default function Home() {
   const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>The Value of count is {count}</h1>
+      <button className="bg-green" onClick={() => dispatch(increament())}>
+        Increament
+      </button>
+      <button className="bg-red" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
     </main>
   );
 }
