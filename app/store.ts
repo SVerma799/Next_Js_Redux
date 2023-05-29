058bec140a99169d3f69e088669f71d348d74b12
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-05-28 23:36:43 -0700

    Changes done to improve the store code. resuability

--- app/store.ts
+++ app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import CounterReducer from "../slice/counterSlice";
 export const store = configureStore({
   reducer: {
-    // counter: CounterReducer,
+    counter: CounterReducer,
   },
 });
 
